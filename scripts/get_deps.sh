7449f7141ff71b5ca346d3c39e0c5a0b05aeb71c
croakexciting <croakexciting@gmail.com>
2025-09-14 21:02:49 +0800

    add sel4 settingS

--- scripts/get_deps.sh
+++ scripts/get_deps.sh
@@ -3,6 +3,6 @@
 AX_ROOT=.arceos
 
 test ! -d "$AX_ROOT" && echo "Cloning repositories ..." || true
-test ! -d "$AX_ROOT" && git clone https://github.com/arceos-org/arceos "$AX_ROOT" --depth=1 || true
+test ! -d "$AX_ROOT" && git clone https://github.com/reL4team2/arceos.git "$AX_ROOT" --branch sel4-porting --depth=1 || true
 
 $(dirname $0)/set_ax_root.sh $AX_ROOT
